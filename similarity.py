284b09948149c32405003d3ee24094b8813c2cf9
vijaykumar-1551 <vijaykumar@p2fsemi.in>
2023-04-29 11:33:04 +0530

    Text similarity and wiki summarizer

---
+++ similarity.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModel.from_pretrained("bert-base-uncased")
+
+st.title("Paragraph Similarity Checker")
+
+# Define the user input fields for the two paragraphs
+paragraph1 = st.text_input("Enter the first paragraph:")
+paragraph2 = st.text_input("Enter the second paragraph:")
+
+# Add a submit button
+if st.button("Check Similarity"):
+    if paragraph1 and paragraph2:
+        # Tokenize the paragraphs and convert to PyTorch tensors
+        inputs1 = tokenizer(paragraph1, padding=True, truncation=True, return_tensors="pt")
+        inputs2 = tokenizer(paragraph2, padding=True, truncation=True, return_tensors="pt")
+
+        # Get the model output for each paragraph
+        outputs1 = model(**inputs1)
+        outputs2 = model(**inputs2)
+
+        # Calculate the cosine similarity between the two output embeddings
+        cos_sim = torch.nn.functional.cosine_similarity(outputs1.last_hidden_state.mean(dim=1), outputs2.last_hidden_state.mean(dim=1))
+
+        # Define a threshold for similarity
+        threshold = 0.8
+
+        # Determine whether the paragraphs are similar or not
+        if cos_sim.item() > threshold:
+            st.write("The two paragraphs are similar.")
+        else:
+            st.write("The two paragraphs are not similar.")
---
+++ wiki-scrap_with _summary.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import re
+from transformers import pipeline 
+from transformers import TFAutoModelForSeq2SeqLM
+import torch
+
+# Function to scrape Wikipedia article and summarize text
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def scrapeWikiArticle(url):
+    response = requests.get(url=url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    title = soup.find(id="firstHeading")
+    st.write(f"Title: {title.text}")
+
+    allLinks = soup.find(id="bodyContent").find_all("p")
+    article = ""
+    for link in allLinks:
+        article += link.text
+
+    # Remove invalid characters
+    article = re.sub(r'[^\x00-\x7F]+', ' ', article)
+
+    # Summarize text
+    def summarizeText(text):
+        model_name = "sshleifer/distilbart-cnn-12-6"
+        summarizer = pipeline("summarization", model = TFAutoModelForSeq2SeqLM.from_pretrained(model_name, from_pt = True), tokenizer="sshleifer/distilbart-cnn-12-6")
+        max_chunk_len = 1024
+        chunks = [text[i:i+max_chunk_len] for i in range(0, len(text), max_chunk_len)]
+        summary = ""
+        for chunk in chunks:
+            summary += summarizer(chunk, max_length=120, min_length=30, do_sample=False)[0]['summary_text'] + " "
+        return summary
+
+    # Save summary to file
+    summary = summarizeText(article)
+    with open('summary.txt', 'w') as f:
+        f.write(summary)
+
+    # Show summary in streamlit
+    st.write("Summary:")
+    st.write(summary)
+
+    # Return summarized text
+    return summary
+
+# Streamlit UI
+st.title("Wikipedia Article Summarizer")
+url = st.text_input("Enter Wikipedia URL:")
+if url:
+    summary = scrapeWikiArticle(url)
+    while True:
+        question = st.text_input("Ask a question (type 'quit' to exit):")
+        if question == "quit":
+            break
+        else:
+            question_answerer = pipeline("question-answering", model="deepset/roberta-base-squad2", tokenizer="deepset/roberta-base-squad2")
+            result = question_answerer(question=question, context=summary)
+            st.write("Answer:")
+            st.write(result['answer'])
